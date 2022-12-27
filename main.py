5f34332ef47de047b3f360aaff6e121a7662aac6
adylbek0408 <adylbek0408@icloud.com>
2022-12-27 14:25:25 +0300

    hw5

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
+import asyncio
+
 from aiogram.utils import executor
 from config import dp
 import logging
-from handlers import client, callback, extra, admin, fsmAdminMentor
+from handlers import client, callback, extra, admin, fsmAdminMentor,notifications
 from database.bot_db import sql_create
 from database import bot_db
 
@@ -10,12 +12,13 @@ fsmAdminMentor.register_handlers_fsm_anketa(dp)
 client.register_handler_client(dp)
 callback.register_handler_callback(dp)
 admin.register_handler_admin(dp)
-
+notifications.register_handlers_notification(dp)
 
 extra.register_handler_extra(dp)
 
 
 async def on_startup(_):
+    asyncio.create_task(notifications.scheduler())
     sql_create()
 
 
